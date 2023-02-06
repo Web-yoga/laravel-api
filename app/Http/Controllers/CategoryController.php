1ad646cad995e621d94e8c446ca1b65ecf273abe
Alexander Tsvetkov <hyehaeng@gmail.com>
2023-02-06 16:42:27 +0300

    category api

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Category;
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Category::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+		$request->validate([
+			'name' => 'required',
+			'slug' => 'required'
+		]);
+
+		return Category::create($request->all());
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return Category::find($id);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $category = Category::find($id);
+		$category->update($request->all());
+		return $category;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        return Category::destroy($id);
+    }
+
+	/**
+     * Search for a name.
+     *
+     * @param  str  $name
+     * @return \Illuminate\Http\Response
+     */
+    public function search($name)
+    {
+        return Category::where('name', 'like', '%'.$name.'%')->get();
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Models\Post;
+use App\Http\Controllers\CategoryController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -15,9 +15,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/posts', function(){
-	return Post::all();
-});
+//Route::resource('category', CategoryController::class);
+Route::get('/category', [ CategoryController::class, 'index' ]);
+Route::post('/category', [ CategoryController::class, 'store' ]);
+Route::get('/category/{id}', [ CategoryController::class, 'show' ]);
+Route::put('/category/{id}', [ CategoryController::class, 'update' ]);
+Route::delete('/category/{id}', [ CategoryController::class, 'destroy' ]);
+Route::get('/category/search/{name}', [ CategoryController::class, 'search' ]);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
